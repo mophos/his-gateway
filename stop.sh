99bb288137d1210d7be5768f49fe3ecd15996c3d
naphattharawat <naphattharawat@gmail.com>
2022-02-14 01:44:36 +0700

    update

--- stop.sh
+++ stop.sh
@@ -1,9 +1,7 @@
 if [  "$(docker ps -a)" ]; then
   if [[ -d "./hisgateway-docker" && -f "./hisgateway-docker/.env"  ]]; then
-    if [[ $option =~ ^(sk)$ && -f "./cert/version" ]]; then
         cd hisgateway-docker
         docker-compose down
-      fi
   else
     echo 'Please confit ENV  ./set-env.sh'
   fi
