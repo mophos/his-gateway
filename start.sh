ca05379974dc8248714088acae5da0c50141f7fa
naphattharawat <naphattharawat@gmail.com>
2022-02-14 01:32:56 +0700

    rename

--- start.sh
+++ start.sh
@@ -6,14 +6,12 @@ if [  "$(docker ps -a)" ]; then
         exit 1
     fi
 
-
-
     if [[ -d "./hisgateway-docker" && -f "./hisgateway-docker/.env"  ]]; then
-    if [[ $option =~ ^(sk)$ && -f "./cert/version" ]]; then
+        if [[ $option =~ ^(sk)$ && -f "./cert/version" ]]; then
             docker-compose -f ./hisgateway-docker/docker-compose.yaml up -d
         else
-        ./update.sh   
-        docker-compose -f ./hisgateway-docker/docker-compose.yaml up -d
+            ./update.sh   
+            docker-compose -f ./hisgateway-docker/docker-compose.yaml up -d
         fi
     else
     echo 'Please confit ENV  ./set-env.sh'
