c38f5e45933282990c7d3f592150d3b70dfb03c0
naphattharawat <naphattharawat@gmail.com>
2022-02-21 15:12:09 +0700

    remove cmd remove docker

--- install.sh
+++ install.sh
@@ -75,7 +75,7 @@ if  [ "$(uname -a | grep el7)" ] || [ "$(uname -a | grep el8)" ] || [ "$(uname -
 
         if ! [ -x "$(command -v docker)" ]; then
             echo 'Installing docker...'
-            sudo apt-get remove docker docker-engine docker.io containerd runc
+            # sudo apt-get remove docker docker-engine docker.io containerd runc 2>/dev/null
             sudo apt-get install ca-certificates curl gnupg lsb-release
             curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
             echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
